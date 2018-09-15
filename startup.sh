ff886b9c9db2941054a0417370d97b5feaad9a6b
Volodymyr Fialko <volodymyrf@interfacemasters.com>
2018-09-15 15:05:05 +0300

    Add auto setup sctipt

---
+++ startup.sh
@@ -0,0 +1,19 @@
+sudo apt install vim vim-gtk git fonts-powerline
+sudo apt-get install python-dev python3-dev build-essential cmake
+echo "Copy dotfiles"
+DOTFILES= ~/git/dotfiles
+ln -s $DOTFILES/.bashrc ~/
+ln -s $DOTFILES/.vimrc ~/
+mkdir ~/.vim/colors
+ln -s $DOTFILES/monokai.vim ~/.vim/colors
+
+echo "Vundle installing..."
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+echo "FZF installing..."
+ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
+echo "YouCompleteMe building"
+cd ~/.vim/bundle/YouCompleteMe
+./install.py --clang-completer
