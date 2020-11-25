4b8bf2cd93b5675943932ab95d11de24113e799a
f0rget <f0rget@pm.me>
2020-11-25 16:37:21 +0200

    add fish config

---
+++ fish/config.fish
@@ -0,0 +1,18 @@
+abbr -a g git
+abbr -a gs "git status"
+abbr -a gf "git diff"
+
+set -U fish_user_paths /home/f0rget/go/bin $fish_user_paths
+
+set -U fish_greeting ""
+
+setenv LIBGL_ALWAYS_SOFTWARE 1
+setenv CARGO_TARGET_DIR ~/.cargo-target
+
+function cd
+    if count $argv > /dev/null
+        builtin cd "$argv"; and ls
+    else
+        builtin cd ~; and ls
+    end
+end
